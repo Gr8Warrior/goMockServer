64ce0a11fe9573588db7b8d546a0099fa46ebba5
Gr8Warrior <shailusuriyal@gmail.com>
2022-12-29 11:34:04 +0530

    Added controller methods

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 // Model for course -file
 type Course struct {
@@ -27,4 +31,17 @@ func (c *Course) isEmpty() bool {
 
 func main() {
 	fmt.Println("Mock server in json")
+}
+
+//controllers - file
+
+// server home route
+func serverHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1> Welcome to Mock API Server </h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
