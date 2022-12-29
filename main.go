ae1a674a4f0c2bd83c32b1040ab3d7775a673bac
Gr8Warrior <shailusuriyal@gmail.com>
2022-12-29 11:27:33 +0530

    Created models- Courses and Author

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+// Model for course -file
+type Course struct {
+	CouserId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake DB
+var courses []Course
+
+// middleware, helper -file
+func (c *Course) isEmpty() bool {
+
+	return c.CouserId == "" && c.CourseName == ""
+
+}
+
 func main() {
-	fmt.Println("Shailu")
+	fmt.Println("Mock server in json")
 }
